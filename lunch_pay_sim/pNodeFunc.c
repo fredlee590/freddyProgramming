0848cdfa3fbb56651b702aad15baecb6145a8cc8
Freddy <fredlee590@gmail.com>
2015-01-01 02:29:31 -0800

    For struct, switch dot syntax for arrow syntax.

--- lunch_pay_sim/pNodeFunc.c
+++ lunch_pay_sim/pNodeFunc.c
@@ -9,12 +9,11 @@
 
 void initParticipant(PNODE* parNodePtr, char* parName)
 {
-	strcpy((*parNodePtr).name, parName);
-	//(*parNodePtr).name = parName;
-	(*parNodePtr).num_lunches = 0;
-	(*parNodePtr).points = 0;
-	(*parNodePtr).spent = 0.0;
-	(*parNodePtr).next = NULL;
+	strcpy(parNodePtr->name, parName);
+	parNodePtr->num_lunches = 0;
+	parNodePtr->points = 0;
+	parNodePtr->spent = 0.0;
+	parNodePtr->next = NULL;
 }
 
 PNODE* addParticipant(PNODE* head, char* parName)
@@ -30,21 +29,21 @@ PNODE* addParticipant(PNODE* head, char* parName)
 		printf("    Adding participant %s\n", parName);
 
 		// get to the end
-		printf("    head: %s\n", (*head).name);
+		printf("    head: %s\n", head->name);
 		printf("    head ptr: %p\n", head);
 		PNODE* current = head;
-		while((*current).next != NULL)
+		while(current->next != NULL)
 		{
-			printf("    passing %s\n", (*current).name);
+			printf("    passing %s\n", current->name);
 			printf("    passing %p\n", current);
-			current = (*current).next;
+			current = current->next;
 		}
 
-		printf("    tail: %s\n", (*current).name);
+		printf("    tail: %s\n", current->name);
 		printf("    tail ptr: %p\n", current);
 		PNODE *newPNODE = malloc(sizeof(PNODE));
 		initParticipant(newPNODE, parName); // todo: move this out to another function for list
-		(*current).next = newPNODE;
+		current->next = newPNODE;
 	}
 	return head;
 }
@@ -63,7 +62,7 @@ unsigned char get_length(PNODE* head)
 		while(current != NULL)
 		{
 			count++;
-			current = (*current).next;
+			current = current->next;
 		}
 		return count;
 	}
@@ -74,12 +73,12 @@ PNODE* join_lists(PNODE* head1, PNODE* head2)
 	// get to end of first list
 	PNODE* current = head1;
 
-	while((*current).next != NULL)
+	while(current->next != NULL)
 	{
-		current = (*current).next;
+		current = current->next;
 	}
 
-	(*current).next = head2;
+	current->next = head2;
 
 	return head1;
 }
@@ -96,11 +95,11 @@ void printList(PNODE* head)
 		while(current != NULL)
 		{
 			printf("Addr: %p\n", current);
-			printf("    Participant: %s\n", (*current).name);
-			printf("    num_lunches: %d\n", (*current).num_lunches);
-			printf("    points: %d\n", (*current).points);
-			printf("    spent: %.2f\n", (*current).spent);
-			current = (*current).next;
+			printf("    Participant: %s\n", current->name);
+			printf("    num_lunches: %d\n", current->num_lunches);
+			printf("    points: %d\n", current->points);
+			printf("    spent: %.2f\n", current->spent);
+			current = current->next;
 		}
 	}
 }
