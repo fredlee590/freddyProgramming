738abfd85bc65ff51f2078fae3679cc46c78b787
Freddy <fredlee590@gmail.com>
2015-01-26 22:12:02 -0800

    implement basic encryption.

--- sillyEncryption/sillyEncryptionFunc.c
+++ sillyEncryption/sillyEncryptionFunc.c
@@ -8,44 +8,49 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 char* sillyEncrypt(char* keyword, char* toEncrypt)
 {
-	#ifdef DEBUG
 	int i;
-	#endif
-
 	MD5_CTX c;
 	unsigned char out[MD5_DIGEST_LENGTH];
 
-	// create default (empty) string
-	char* result = "";
+	// set up defaults
+	char sign = 1;
+
+	char* result = malloc(sizeof(char)*strlen(toEncrypt));
 
 	// compute md5sum of keyword
 	unsigned long keyword_len = strlen(keyword);
+	unsigned int toEncrypt_len = strlen(toEncrypt);
+
+	strcpy(result, toEncrypt);
 
 	MD5_Init(&c);
 	MD5_Update(&c, keyword, keyword_len);
 
 	MD5_Final(out, &c);
 
-	#ifdef DEBUG
-	for(i = 0; i < MD5_DIGEST_LENGTH; i++)
+	// walk down each part of the md5sum
+	for(i = 0; i < toEncrypt_len; i++)
 	{
-		printf("%02x", out[i]);
+		// compute offset from md5sum part
+		unsigned char offset = out[i] & 0xF;
+
+		char newChar = result[i] + (sign * offset);
+
+		printf("%c %c\n", result[i], newChar);
+
+		// add or subtract md5sum part to or from string to encrypt
+		result[i] = newChar;
+
+		sign *= -1;
 	}
 
+	#ifdef DEBUG
 	printf("\n");
 	#endif
-
-	// walk down each part of the md5sum (bitmask)
-	// get corresponding part of string to encrypt
-
-	// compute offset from md5sum part
-	// add or subtract md5sum part to or from string to encrypt
-	// append to result string
-
-	// return result
 
 	return result;
 }
