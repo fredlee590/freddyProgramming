4d098898718628c9b12a4c8b9dc187f86fbed245
Freddy <fredlee590@gmail.com>
2016-07-27 02:13:33 -0700

    Initial commit of projectRome ciphertext analysis tool.

---
+++ go/src/projectRome/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Need exactly two arguments: to describe file name")
+		fmt.Println("1) File name of cipher text")
+		fmt.Println("2) Command to execute")
+		fmt.Println("Exiting!")
+		os.Exit(1)
+	}
+
+	fmt.Println("=====================================================================================")
+	fmt.Printf("Beginning a run of %s, the cipher analysis tool, with the following parameters.\n", os.Args[0])
+	fileName := os.Args[1]
+	command := os.Args[2]
+	fmt.Printf("File name: %s\n", fileName)
+	fmt.Printf("Command: %s\n", command)
+	fmt.Println("=====================================================================================")
+
+	fmt.Println()
+
+	// validate file name
+	if _, err := os.Stat(fileName); err != nil {
+		fmt.Printf("Failed to stat file %s:\n(%s)\n", fileName, err)
+		os.Exit(2)
+	}
+
+	// validate command
+	switch command {
+	case "print":
+		fmt.Println("Printing file")
+	default:
+		fmt.Println("Unknown command")
+		os.Exit(3)
+	}
+}
